2fe80028fff0bde787019313465f5b38036f3e8e
Shubham Bhardwaj <shubham200015@yahoo.com>
2021-05-30 23:43:27 +0530

    Add navbar component

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import '../Styles/NavBar.css'
+
+export class NavBar extends Component {
+    render() {
+        return (
+            <div className="NavBar">
+                <h2 className="NavBar-heading">Navbar</h2>
+            </div>
+        )
+    }
+}
+
+export default NavBar
