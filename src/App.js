cb8c840231e5386a8a95ad3add050561859c891e
Shubham Bhardwaj <shubham200015@yahoo.com>
2021-05-31 17:11:14 +0530

    Collecting data from the API and passing on to the graph component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import './App.css';
+import React, { Component } from 'react';
 import NavBar from './Components/NavBar';
+import Graph from './Components/Graph';
+import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar />
-    </div>
-  );
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    }
+    this.handleData = this.handleData.bind(this);
+  }
+  handleData(data) {
+    this.setState({
+      data: [data.tradeSeries, data.importSeries, data.exportSeries, data.tradeDeficitSeries]
+    });
+  }
+
+  componentDidMount() {
+    axios.get("https://run.mocky.io/v3/3be6c19d-7ae5-4f84-950c-5b4ab4b22537")
+      .then(res => {
+        const data = res.data;
+        // console.log(data.exportSeries, data.tradeSeries, data.importSeries, data.tradeDeficitSeries);
+        this.handleData(data);
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        <div className="container">
+          {this.state.data.map((parameter) => (
+            <Graph data={parameter} key={parameter} />
+          ))}
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/Components/Graph.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import '../Styles/Graph.css'
+
+export class Graph extends Component {
+    render() {
+        return (
+            <div className="Graph">
+                <h1>Graph goes here</h1>
+            </div>
+        )
+    }
+}
+
+export default Graph
